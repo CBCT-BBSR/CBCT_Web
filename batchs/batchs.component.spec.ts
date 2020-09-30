bb98f7754bf7e44d55597fee905769d5cc550d07
LovelySahoo <53365601+LovelySahoo@users.noreply.github.com>
2020-09-30 10:57:44 +0530

    batch api calling in web

---
+++ batchs/batchs.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BatchsComponent } from './batchs.component';
+
+describe('BatchsComponent', () => {
+  let component: BatchsComponent;
+  let fixture: ComponentFixture<BatchsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ BatchsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BatchsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
